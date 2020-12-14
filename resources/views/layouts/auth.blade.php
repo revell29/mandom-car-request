572f99437c7aa74bce883d85a75de4db3a4893b1
revell29 <diraapsya0@gmail.com>
2020-12-14 22:12:37 +0700

    remove vertilogic

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>{{env('APP_NAME')}} &dash; Vertilogic</title>
+    <title>{{env('APP_NAME')}} &dash;</title>
 
     <!-- Global stylesheets -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet"
